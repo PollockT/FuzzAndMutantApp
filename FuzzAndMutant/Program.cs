33cd6a5fb5e9414882c27a50bb984aee6a0e8d36
Theodore Pollock <pollockt@tulsapretech.com>
2021-07-12 21:53:58 -0500

    Add project files.

---
+++ FuzzAndMutant/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FuzzAndMutant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = args[0];
+            int index = url.IndexOf("?");
+            string
+        }
+    }
+}
