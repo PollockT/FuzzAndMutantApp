719f79ee282eb39ad3408d3e67d6a36722505118
Theodore Pollock <pollockt@tulsapretech.com>
2021-07-15 20:23:45 -0500

    basic Sql and XSS hunter added

--- FuzzAndMutant/Program.cs
+++ FuzzAndMutant/Program.cs
@@ -8,7 +8,15 @@ namespace FuzzAndMutant
         {
             string url = args[0];
             int index = url.IndexOf("?");
-            string
+            string[] parms = url.Remove(0, index + 1).Split('&');
+            foreach (string parm in parms)
+            {
+                string xssUrl = url.Replace(parm, parm + "fd<xss>sa");
+                string sqlUrl = url.Replace(parm, parm + "fd'sa");
+
+                Console.WriteLine(xssUrl);
+                Console.WriteLine(sqlUrl);
+            }
         }
     }
 }
